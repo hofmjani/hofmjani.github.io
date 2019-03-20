7733c846cca41b2aa082e1009f2a3dcb65719c00
hofmjani <csav6794@student.uibk.ac.at>
2019-03-20 18:14:23 +0100

    Ausschnitt setzen

--- nz/main.js
+++ nz/main.js
@@ -1,15 +1,22 @@
 // alert("hello world!");
 //hier: deutsche variablen verwenden (in html englische variablen)
 
-const div= document.getElementById("map");
-const breite=div.getAttribute("data-lat");
-const laenge=div.getAttribute("data-lng");
-const titel=div.getAttribute("data-title");
+const div = document.getElementById("map");
+const breite = div.getAttribute("data-lat");
+const laenge = div.getAttribute("data-lng");
+const titel = div.getAttribute("data-title");
 
 
 //console.log("Breite=",breite,"Länge=",länge,"Titel=",titel);
 
 //Karte initialisieren 
 
-let karte= L.map("map");
+let karte = L.map("map");
 //console.log(karte);
+
+
+//auf ausschnitt zoomen: 
+karte.setView(
+    [breite, laenge], 13
+);
+
