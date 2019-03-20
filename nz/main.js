1bad13c8f762b253a9fd10855cc484abae062a60
hofmjani <csav6794@student.uibk.ac.at>
2019-03-20 18:11:58 +0100

    Karte initialisiern und variablen auf deutsch

--- nz/main.js
+++ nz/main.js
@@ -1,10 +1,15 @@
 // alert("hello world!");
+//hier: deutsche variablen verwenden (in html englische variablen)
 
 const div= document.getElementById("map");
-const lat=div.getAttribute("data-lat");
-const lng=div.getAttribute("data-lng");
-const title=div.getAttribute("data-title");
+const breite=div.getAttribute("data-lat");
+const laenge=div.getAttribute("data-lng");
+const titel=div.getAttribute("data-title");
 
 
-//console.log("Breite=",lat,"Länge=",lng,"Titel=",title);
+//console.log("Breite=",breite,"Länge=",länge,"Titel=",titel);
 
+//Karte initialisieren 
+
+let karte= L.map("map");
+//console.log(karte);
