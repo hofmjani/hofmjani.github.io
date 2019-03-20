bb4511d904fe005570799c09b58b81aebac96570
hofmjani <csav6794@student.uibk.ac.at>
2019-03-20 18:04:58 +0100

    leaflet einbinden

---
+++ nz/main.js
@@ -0,0 +1,10 @@
+// alert("hello world!");
+
+const div= document.getElementById("map");
+const lat=div.getAttribute("data-lat");
+const lng=div.getAttribute("data-lng");
+const title=div.getAttribute("data-title");
+
+
+//console.log("Breite=",lat,"Länge=",lng,"Titel=",title);
+
