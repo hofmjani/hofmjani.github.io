26d99f3801b505556c3fa91a53633ea0cd471129
hofmjani <csav6794@student.uibk.ac.at>
2019-03-21 18:18:58 +0100

    10/10 weitere Challenges

--- main.js
+++ main.js
@@ -1,6 +1,28 @@
-let newVariable = 'Playing around with typeof.';
+let test = 'Versuch'
+console.log(test)
+test = 'Versuch2'
+console.log(test)
 
-console.log(typeof (newVariable))
-newVariable = 1
 
-console.log(typeof (newVariable))
+const test1 = 'Versuch'
+console.log(test1)
+//test1='Versuch2'
+// const lsst Variabeln nicht mehr ndern!
+
+var test3 = 'Versuch'
+console.log(test3)
+test3 = 'Versuch2'
+console.log(test3)
+//var lsst variablen ndern, aber veraltet! 
+
+console.log(typeof (test3))
+
+let Nummer = 234453
+console.log(typeof (Nummer))
+
+let bool = true
+console.log(typeof (bool))
+
+let dataTypeMix = 2 'Euro'
+console.log(typeof (dataTypeMix))
+// funktioniert nicht 
