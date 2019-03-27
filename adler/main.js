f98addd7c101d7fbb7cf73119946499be80a5cb1
hofmjani <csav6794@student.uibk.ac.at>
2019-03-27 16:01:49 +0100

    erste schritte css und js

---
+++ adler/main.js
@@ -0,0 +1,36 @@
+// alert("hello world!");
+//hier: deutsche variablen verwenden (in html englische variablen)
+
+const div = document.getElementById("map");
+const breite = div.getAttribute("data-lat");
+const laenge = div.getAttribute("data-lng");
+const titel = div.getAttribute("data-title");
+
+
+//console.log("Breite=",breite,"Länge=",länge,"Titel=",titel);
+
+//Karte initialisieren 
+
+let karte = L.map("map");
+//console.log(karte);
+
+
+//auf ausschnitt zoomen: 
+karte.setView(
+    [breite, laenge], 13
+);
+
+//open street map einbinden
+
+L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png").addTo(karte);
+
+//Positionsmarker hinzufügen
+let pin= L.marker(
+    [breite,laenge]
+).addTo(karte);
+
+
+//Popup zum Pin hängen
+pin.bindPopup(titel).openPopup();
+
+
