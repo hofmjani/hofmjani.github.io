d907d53f05f124fa0963e259834dec5031aa8060
hofmjani <csav6794@student.uibk.ac.at>
2019-03-27 15:33:09 +0100

    Ordnerstruktur verändert

---
+++ workload/3/main.js
@@ -0,0 +1,29 @@
+let test = 'Versuch'
+console.log(test)
+test = 'Versuch2'
+console.log(test)
+
+
+const test1 = 'Versuch'
+console.log(test1)
+//test1='Versuch2'
+// const lsst Variabeln nicht mehr ndern!
+
+var test3 = 'Versuch'
+console.log(test3)
+test3 = 'Versuch2'
+console.log(test3)
+//var lsst variablen ndern, aber veraltet! 
+
+console.log(typeof (test3))
+
+let Nummer = 234453
+console.log(typeof (Nummer))
+
+let bool = true
+console.log(typeof (bool))
+
+
+let dataTypeMix = 2 'Euro'
+console.log(typeof (dataTypeMix))
+// funktioniert nicht 
