efaefc26b666372eaa00d3ee755efa85664d1efb
hofmjani <csav6794@student.uibk.ac.at>
2019-04-03 16:17:42 +0200

    Karte ändern

--- adler/main.js
+++ adler/main.js
@@ -27,7 +27,17 @@ karte.setView(
 
 //open street map einbinden
 
-L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png").addTo(karte);
+//L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
+   // subdomains: ["a","b","c"], 
+   // attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
+//}).addTo(karte); 
+
+
+
+L.tileLayer("https://{s}.wien.gv.at/basemap/geolandbasemap/normal/google3857/{z}/{y}/{x}.png",{
+    subdomains : ["maps", "maps1","maps2", "maps3", "maps4"],
+    attribution : 'Datenquelle: <a href="https://www.basemap.at">basemap.at</a>'
+}).addTo(karte);
 
 //Positionsmarker hinzufügen
 let pin1 = L.marker(
